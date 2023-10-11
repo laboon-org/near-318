4768df7387d04fcf7fc6badc3d63d21bbfeddbdd
Hiep Le <>
2023-10-11 22:49:12 +0700

    config: [api] dotenv load more variables

--- back/src/near-contract.js
+++ back/src/near-contract.js
@@ -1,34 +1,41 @@
 import * as nearAPI from "near-api-js";
 import "dotenv/config";
 
-const {keyStores, KeyPair, connect, Contract} = nearAPI;
+const { keyStores, KeyPair, connect, Contract } = nearAPI;
 
-const TGAS = 300000000000000;
-const NO_DEPOSIT = 0;
-const NO_ARGS = {};
+const {
+  TGAS,
+  NO_DEPOSIT,
+  NO_ARGS,
+  CONTRACT_ADDRESS,
+  NEAR_NETWORK,
+  PRIVATE_KEY,
+  NODE_URL,
+  WALLET_URL,
+  HELPER_URL,
+  EXPLORER_URL,
+} = process.env;
 
-const contractAddress = process.env.CONTRACT_ADDRESS;
 const triggerContract = async() => {
   const myKeyStore = new keyStores.InMemoryKeyStore();
-  const { PRIVATE_KEY } = process.env;
   const keyPair = KeyPair.fromString(PRIVATE_KEY);
-  await myKeyStore.setKey("testnet", contractAddress, keyPair);
+  await myKeyStore.setKey(nearNetwork, CONTRACT_ADDRESS, keyPair);
   // console.log(myKeyStore);
 
   const connectionConfig = {
-    networkId: "testnet",
+    networkId: NEAR_NETWORK,
     keyStore: myKeyStore,
-    nodeUrl: "https://rpc.testnet.near.org",
-    walletUrl: "https://wallet.testnet.near.org",
-    helperUrl: "https://helper.testnet.near.org",
-    explorerUrl: "https://explorer.testnet.near.org",
+    nodeUrl: NODE_URL,
+    walletUrl: WALLET_URL,
+    helperUrl: HELPER_URL,
+    explorerUrl: EXPLORER_URL,
   };
 
   const nearConnection = await connect(connectionConfig);
-  const account = await nearConnection.account(contractAddress);
+  const account = await nearConnection.account(CONTRACT_ADDRESS);
   const contract = new Contract(
     account, // the account object that is connecting
-    contractAddress,
+    CONTRACT_ADDRESS,
     {
       // name of contract you're connecting to
       viewMethods: ["get_tickets"], // view methods do not change state but usually return a value
@@ -37,7 +44,7 @@ const triggerContract = async() => {
   );
 
   const response = await contract.proceed_bingo(NO_ARGS, TGAS, NO_DEPOSIT);
-  console.log(response);
+  // console.log(response);
 }
 
 export default triggerContract;
